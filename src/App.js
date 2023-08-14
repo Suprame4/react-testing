2e295df565eb9316cde6913dd6990819aa082a9b
Adrian G <adriancortesgonz@gmail.com>
2023-08-13 20:58:10 -0700

    complete section a

--- src/App.js
+++ src/App.js
@@ -3,6 +3,11 @@ import React, { useState } from 'react';
 function App() {
   const [theme, setTheme] = useState('light');
   const [showHidden, setShowHidden] = useState(false);
+  const [search, setSearch] = useState('');
+
+  function handleChange (event) {
+    setSearch(event.target.value);
+  }
 
   function toggleTheme() {
     const nextTheme = theme === 'dark' ? 'light' : 'dark';
@@ -13,6 +18,20 @@ function App() {
     setShowHidden(!showHidden);
   }
 
+  function Search({ value, onChange, children }) {
+    return (
+      <div>
+        <label htmlFor="search">{children}</label>
+        <input
+          id="search"
+          type="text"
+          value={value}
+          onChange={onChange}
+        />
+      </div>
+    );
+  }
+
   const color = theme === 'light' ? '#333' : '#FFF';
   const backgroundColor = theme === 'light' ? '#FFF' : '#333';
 
@@ -21,22 +40,32 @@ function App() {
 
   return (
     <div className='container'>
-      <h1>Welcome, party people!</h1>
-      <p>Click the button to toggle the theme</p>
+      <div>
+        <h1>Welcome, party people!</h1>
+        <p>Click the button to toggle the theme</p>
 
-      <button onClick={toggleTheme}>Current theme: {theme}</button>
+        <button onClick={toggleTheme}>Current theme: {theme}</button>
 
-      <button onClick={toggleHidden}>
-        {showHidden ? 'Hide' : 'Show'} hidden content
-      </button>
+        <button onClick={toggleHidden}>
+          {showHidden ? 'Hide' : 'Show'} hidden content
+        </button>
 
-      {showHidden && (
-        <div>
-          <p>this content is hidden by default</p>
-        </div>
-      )}
+        {showHidden && (
+          <div>
+            <p>this content is hidden by default</p>
+          </div>
+        )}
+      </div>
+      <div>
+        <Search value={search} onChange={handleChange}>
+          Search:
+        </Search>
+
+        <p>Searches for {search ? search : '...'}</p>
+      </div>
     </div>
   );
 }
+
 
 export { App };
--- src/App.test.js
+++ src/App.test.js
@@ -10,6 +10,9 @@ import { App } from './App';
 test('App should render', () => {
   render(<App />);
 
+  // see the HTML output with debug()
+  screen.debug();
+
   expect(screen.getByText('Welcome, party people!')).toBeInTheDocument();
 });
 
@@ -56,3 +59,67 @@ test('hidden button should toggle hidden content', () => {
  *   - check the for the class name .container on the surrounding div
  *   - after clicking the toggle hidden content button, check for the button text to update to "hide" instead of "show"
  */
+
+
+/** 
+ *  REACT TESTING LIBRARY 
+ *
+ */
+describe( 'App', () => {
+
+  it('render App component', () => {
+
+    // Arrange 
+    render(<App/>);
+
+    // Assert
+    // implicit assertion
+    // because getByText would throw error
+    // if element wouldn't be there
+    screen.getByText('Search:');
+
+    // explicit assertion
+    // recommended
+    expect(screen.getByText('Search:')).toBeInTheDocument();
+
+    /*
+      the getByText function accepts a string as an argument, but also a regular expression
+
+        Whereas a string argument is used for the exact match, a regular expression can be
+        used for a partial match which is often more convenient 
+    */
+
+    // fails
+    expect(screen.getByText('Search')).toBeInTheDocument();
+
+    // succeeds
+    expect(screen.getByText('Search:')).toBeInTheDocument();
+
+    // succeeds
+    expect(screen.getByText(/Search/)).toBeInTheDocument();
+  })
+
+  /**
+   * SEARCH Types
+   *    Text is often the common way to select elements with React Testing Library,
+   *    another strong is Role with getByRole
+   * 
+   *    The getByRole function is usually used to retrieve elements by aria-label attributes.
+   *    However, there are also implicit roles on HTML elements -- like button for a button
+   *    element 
+   * 
+   *      The neat thing about getByRole: it shows all the selectable roles if you provide a role
+   *      that isn't available in the rendered component's HTML
+   */
+  it('getByRole search type', () => {
+    
+    // Arrange
+    render(<App/>);
+
+
+    //implicit assert
+    screen.getByRole('');
+  })
+
+  
+})
